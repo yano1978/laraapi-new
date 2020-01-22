121521b7b3321be095ec6d73f5b70d8b657ef3d3
eianuari <yano1978@gmail.com>
2020-01-22 09:25:54 +0100

    UI integration for login + UsersController test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email', 32)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+        // Optionals columns
+            $table->string('lastname', 32)->nullable();
+            $table->string('phone', 16)->nullable();
+            $table->string('province', 32)->nullable();
+            $table->smallInteger('age', false, true)->nullable();
         });
     }
 
