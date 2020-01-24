a76639465e233068ce898202841c30492326dbdf
ernestoianuario <yano1978@gmail.com>
2020-01-24 17:58:30 +0100

    JWT integration ok (with FrontEnd - login ONLY)

--- app/User.php
+++ app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -18,6 +18,26 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password', 'lastname', 'phone', 'province','fiscalcode', 'age'
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return ['name' => $this->name,
+                'email' => $this->email
+                ];
+    }
 
     /**
      * The attributes that should be hidden for arrays.
